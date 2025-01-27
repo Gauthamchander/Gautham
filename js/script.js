a28355d74db0ab758849302112381b5f8527ce46
Gautham <gauthamkraze@gmail.com>
2025-01-27 19:39:49 +0530

    new file

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 
   const header = document.getElementById('sticky-header');
+  let lastScrollY = 0;
     
   window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
     // Check if the user has scrolled beyond the first fold (e.g., 150px)
-    if (window.scrollY > 100) {
+    if (window.scrollY > 200) {
       header.classList.add('scrolled'); // Add the class for the new background
     } else {
       header.classList.remove('scrolled'); // Remove the class when scrolled back up
     }
+    lastScrollY = currentScrollY;
   });
 
   const scrollContainer = document.querySelector('.image-scroll');
